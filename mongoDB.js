7d3c566940f0966206216591eb7352db7ab5e12b
RishiRao123 <119345554+RishiRao123@users.noreply.github.com>
2024-04-25 23:42:33 +0530

    Add files via upload

---
+++ mongoDB.js
@@ -0,0 +1,33 @@
+const mongoose=require('mongoose')
+
+mongoose.connect("mongodb://localhost:27017/EazyDine")
+.then(()=>{
+    console.log("Connected Successfully");
+})
+.catch(()=>{
+    console.log("Error in connecting to DB");
+})
+
+const LogInSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required: true
+        
+    },
+    email:{
+        type:String,
+        
+    },
+    phone:{
+        type:Number,
+       
+    },
+    password:{
+        type:String,
+       
+    }
+},{ collection: 'Users' });
+
+
+const LogInCollection=new mongoose.model("LogIn",LogInSchema)
+module.exports=LogInCollection;
